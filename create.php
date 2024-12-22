a215401899d915a73d88051c527ea5a8a66fb32d
provash10 <provash20cb@gmail.com>
2024-12-23 03:12:37 +0600

    first commit

---
+++ create.php
@@ -0,0 +1,12 @@
+<!-- For Data Insert create >>> create.php-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
